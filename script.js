5de6698a9165c6858fbaf522f4352161240e0f82
pauloozildo-sys <pauloozildo@gmail.com>
2026-07-01 03:44:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ document.getElementById('exportarBtn').addEventListener('click', async function(
     const combinedStream = new MediaStream([videoTrack, audioTrack]);
     const mediaRecorder = new MediaRecorder(combinedStream, {
       mimeType: 'video/webm;codecs=vp8,opus',
-      videoBitsPerSecond: 16000000 
+      videoBitsPerSecond: 21000000 
     });
     
     let chunks = [];
